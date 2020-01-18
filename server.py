b72c0c6796e234956ae8f94a75213abd5c96a28d
csoth25 <tsoth25@gmail.com>
2020-01-18 15:48:21 -0800

    Revert commit: 8d1a51a

--- server.py
+++ server.py
@@ -4,9 +4,11 @@ import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = ''
 
 @app.route("/")
 def index():
+    # return "welcome to schedule fighter"
     return render_template('index.html')
 
 
