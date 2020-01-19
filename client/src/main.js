a6bfd08101afb7b6a32802d2973afea0869f0c3d
Huyen Pham <thanhhuyen2995@gmail.com>
2020-01-19 04:46:34 -0800

    added classes to Add page

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 import VueMaterial from 'vue-material'
@@ -9,5 +10,6 @@ import 'vue-material/dist/theme/default.css'
 Vue.use(VueMaterial)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
