d6a1b628e6a35130e50d630fd990441d17f05387
mesagoh <mesagoh@gmail.com>
2020-01-18 18:33:27 -0800

    added vue

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 # import Flask class
 from flask import Flask
-from flask_react import React
+
 # set app variable to an instance of Flask class
 # __name__ is a special name in Python, which is the name of module
 app = Flask(__name__)
-react = React(
-   app,
-   extensions=("material", "components"),
-   jsx_folder='static/react/jsx/'
-)
 
 @app.route('/') # homepage
 def hello_world():
---
+++ client/src/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="banner">
+        <div class="header">
+        <span id="first">Schedule</span><span id="second">Fighter</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Banner'
+}
+</script>
+
+<style scoped>
+
+.header {
+    font-size: 50px;
+}
+
+#first {
+    color: #002855;
+}
+
+#second {
+    color: #B3A369;
+    font-weight: bold;
+}
+
+</style>
